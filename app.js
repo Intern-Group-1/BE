199b74b0ef7887faa85ce6177b6ab4533ca41e9c
Shawn <nhatquangta2911@gmail.com>
2022-02-07 13:49:13 +0700

    Tested git from a Shawn

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const dotenv = require('dotenv')
-const Router = require('./src/routers/api')
-dotenv.config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Router = require("./src/routers/api");
+dotenv.config();
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var db = process.env.MONGO
+var db = process.env.MONGO;
+console.log(1);
 mongoose
   .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB connected!'))
-  .catch((err) => console.log(err))
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err));
 
-app.use('/api', Router)
+app.use("/api", Router);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    
   console.log(`Server is running on port ${PORT}`);
 });
